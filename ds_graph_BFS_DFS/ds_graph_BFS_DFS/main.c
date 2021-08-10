0e2e9c066b96a7583c329f3c9a69ad7f68697be3
himanchalsingh93@gmail.com <himanchalsingh93@gmail.com>
2021-08-10 10:54:28 +0530

    added header file

--- ds_graph_BFS_DFS/ds_graph_BFS_DFS/main.c
+++ ds_graph_BFS_DFS/ds_graph_BFS_DFS/main.c
@@ -13,20 +13,21 @@ void breadth_first_search(int graph[][7], int start, int n){
     int i = start;
     struct Queue_h q;
     int visitiedArray[7] = {0};
-    printf("%d", i);// printing the starting vertex
+    printf("%d", i);//printing the starting vertex
     visitiedArray[i] = 1;
     enqueue(i);
-    
 }
 
 int main(int argc, const char * argv[]) {
-    int Graph[7][7]={{0,0,0,0,0,0,0},
-    {0,0,1,1,0,0,0},
-    {0,1,0,0,1,0,0},
-    {0,1,0,0,1,0,0},
-    {0,0,1,1,0,1,1},
-    {0,0,0,0,1,0,0},
-    {0,0,0,0,1,0,0}};
+    int Graph[7][7] = {
+        {0,0,0,0,0,0,0},
+        {0,0,1,1,0,0,0},
+        {0,1,0,0,1,0,0},
+        {0,1,0,0,1,0,0},
+        {0,0,1,1,0,1,1},
+        {0,0,0,0,1,0,0},
+        {0,0,0,0,1,0,0}
+    };
     printf("Hello, World!\n");
     return 0;
 }
