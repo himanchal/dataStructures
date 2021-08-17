6060112dd246e5f6db434ce0c8902680be44652b
himanchalsingh93@gmail.com <himanchalsingh93@gmail.com>
2021-08-17 10:41:39 +0530

    graph prim's algo

--- ds_graph_prims_algo/ds_graph_prims_algo/main.c
+++ ds_graph_prims_algo/ds_graph_prims_algo/main.c
@@ -1,7 +1,7 @@
-
 #include <stdio.h>
+#include <limits.h>
 #define SIZE 8
-#define I 32767
+#define I INT_MAX
 
 int cost_matrix [SIZE][SIZE] = {
         {I, I, I, I, I, I, I, I},
@@ -14,7 +14,7 @@ int cost_matrix [SIZE][SIZE] = {
         {I, I, 10, I, 14, 18, I, I},
 };
 
-int near[SIZE] = {I, I, I, I, I, I, I, I};
+int near[8] = {I, I, I, I, I, I, I, I};
 int spanning_tree[2][6];
 
 
@@ -24,7 +24,7 @@ int main(int argc, const char * argv[]) {
     int n = 7, min = I, i, j;
     
     //to find the minimum cost edge from cost_matrix
-    for (int i = 1; i <= n; i++) {
+    for (i = 1; i <= n; i++) {
         for (j = 1; j <= n; j++) { //considering only upper triangle
             if (cost_matrix[i][j] < min) {
                 min = cost_matrix[i][j];
@@ -42,7 +42,7 @@ int main(int argc, const char * argv[]) {
     near[u] = near[v] = 0;
     
     //update the near array
-    for (i = 1; i<n; i++) {
+    for (i = 1; i <= n; i++) {
         if (near[i] != 0) {
             if (cost_matrix[i][u] < cost_matrix[i][v])
                 near[i] = u;
@@ -53,7 +53,7 @@ int main(int argc, const char * argv[]) {
     
     for (i = 1; i < n-1; i++) {
         min = I;
-        for (j = 1; j < n; j++) {
+        for (j = 1; j <= n; j++) {
             if (near[j] != 0 && cost_matrix[j][near[j]] < min ) {
                 min_edge = j; // k will have the minimum value
                 min = cost_matrix[j][near[j]];
@@ -65,13 +65,13 @@ int main(int argc, const char * argv[]) {
         
         //update near array to check whether other vertices are nearer to min_edge or not
         
-        for (j = 1; j <=n; j++) {
+        for (j = 1; j<=n; j++) {
             if (near[j] != 0 && cost_matrix[j][min_edge] < cost_matrix[j][near[j]]) {
                 near[j] = min_edge;
             }
         }
-        for (i = 0; i < n; i++) {
-            printf("%d, %d", spanning_tree[0][i], spanning_tree[1][i]);
+        for (i = 0; i < n-1; i++) {
+            printf("%d, %d\n", spanning_tree[0][i], spanning_tree[1][i]);
         }
     }
     return 0;
